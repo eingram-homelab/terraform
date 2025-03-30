88a7283d9af3158cf98c7b040c2a45564a27deee
Edward Ingram <edward.ingram23@gmail.com>
2025-03-29 20:48:45 -0700

    set cred

--- projects/vsphere-tmpl-verify/backend.tf
+++ projects/vsphere-tmpl-verify/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket      = "yc-srv1-tfstate"
-    credentials = "/Users/edwardingram/keys/gcp_bucket_tfstate.json"
+    credentials = "/tmp/cred.json"
     prefix      = "terraform/state/vsphere-vm-test"
   }
 }
