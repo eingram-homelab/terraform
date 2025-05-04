9cb8f30170c323965255647d0780d31ddb90c11e
Edward Ingram <edward.ingram23@gmail.com>
2025-05-03 23:38:53 -0700

    update outputs

--- modules/rancher/outputs.tf
+++ modules/rancher/outputs.tf
@@ -10,10 +10,13 @@ output "cluster_id" {
   value = rancher2_cluster_v2.cluster.id
 }
 
-output "cluster_kube_config" {
-  description = "Kube Config"
-  value     = rancher2_cluster_v2.cluster.kube_config
-  sensitive = true
+# output "cluster_kube_config" {
+#   description = "Kube Config"
+#   value     = rancher2_cluster_v2.cluster.kube_config
+#   sensitive = true
+# }
+
+output "cluster_all" {
+  description = "Cluster All"
+  value       = rancher2_cluster_v2.cluster
 }
-
-
