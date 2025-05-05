90fbd221d2f041113cda5a41502be55e0590a811
Edward Ingram <edward.ingram23@gmail.com>
2025-05-05 16:49:53 -0700

    fmt

--- projects/rancher/k3s-clus1/main.tf
+++ projects/rancher/k3s-clus1/main.tf
@@ -50,11 +50,11 @@ module "rancher" {
   rancher_insecure              = true
   vsphere_cloud_credential_name = "vsphere"
   # cluster_cni = "flannel"
-  disabled_features = ["servicelb", "traefik"]
-  vsphere_tags      = ["dev"]
-  vsphere_cfgparam  = ["disk.enableUUID=TRUE"]
-  salt_password     = data.vault_generic_secret.salt_password.data["salt_password"]
-  ssh_key           = data.vault_generic_secret.ssh_pub_key.data["ssh_pub_key"]
-  tls_san           = ["k3s-clus1.local.lan", "10.10.0.207"]
+  disabled_features     = ["servicelb", "traefik"]
+  vsphere_tags          = ["dev"]
+  vsphere_cfgparam      = ["disk.enableUUID=TRUE"]
+  salt_password         = data.vault_generic_secret.salt_password.data["salt_password"]
+  ssh_key               = data.vault_generic_secret.ssh_pub_key.data["ssh_pub_key"]
+  tls_san               = ["k3s-clus1.local.lan", "10.10.0.207"]
   serialize_image_pulls = true
 }
