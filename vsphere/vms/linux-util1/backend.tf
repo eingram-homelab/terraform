b9ab0850774d0153619b7bc8a5e05c4a984a6ca9
Edward Ingram <edward.ingram23@gmail.com>
2026-06-30 14:05:59 -0700

    update vms

--- vsphere/vms/linux-util1/backend.tf
+++ vsphere/vms/linux-util1/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket      = "yc-srv1-tfstate"
-    prefix      = "terraform/state/vsphere-linux-util1"
+    bucket = "yc-srv1-tfstate"
+    prefix = "terraform/state/vsphere-linux-util1"
   }
 }
