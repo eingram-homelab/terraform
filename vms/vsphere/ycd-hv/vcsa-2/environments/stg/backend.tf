59e40428191a0d22cd7ea7468eacecc71f91d611
Edward Ingram <edward.ingram23@gmail.com>
2026-09-05 11:57:30 -0700

    feat: add vms (#71)

--- vms/vsphere/ycd-hv/vcsa-2/environments/stg/backend.tf
+++ vms/vsphere/ycd-hv/vcsa-2/environments/stg/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "yc-srv1-tfstate"
-    prefix = "terraform/vms/vsphere/ycd-hv1/vcsa-2/environments/stg"
+    prefix = "terraform/vms/vsphere/ycd-hv/vcsa-2/environments/stg"
   }
 }
