746d3e7aaac5e926a4ce032d8ff7126f0e980a51
Edward Ingram <edward.ingram23@gmail.com>
2026-09-20 18:03:58 -0700

    add backend

--- vms/vsphere/linux-rke2/vcsa-2/environments/stg/backend.tf
+++ vms/vsphere/linux-rke2/vcsa-2/environments/stg/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "yc-srv1-tfstate"
-    prefix = "terraform/vms/vsphere/linux-rke2-clus1"
+    prefix = "terraform/vms/vsphere/linux-rke2/environments/stg"
   }
 }
